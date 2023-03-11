d76facb9d4e0aee6b1cfe4e023534b11d43d630f
Vova Soroka <vsoroka@oroinc.com>
2023-03-11 17:06:47 +0200

    BAP-21930: Refactor DI extensions for bundles (#35041)
    - add getSettings() method to Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder that simplifies loading of system settings into DIC in DI extension
    - refactor loading of system settings into DIC in DI extensions
    - add strict type to DI extensions
    - refactor unit tests for DI extensions

--- src/Oro/Bundle/StripeBundle/DependencyInjection/OroStripeExtension.php
+++ src/Oro/Bundle/StripeBundle/DependencyInjection/OroStripeExtension.php
@@ -5,19 +5,16 @@ namespace Oro\Bundle\StripeBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * Load bundle basic configuration files.
- */
 class OroStripeExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(
-            dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'config'
-        ));
-
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form_types.yml');
         $loader->load('method.yml');
