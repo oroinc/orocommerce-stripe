05869cf0cf17ce683115cbd0c7e331a234571f93
Vova Soroka <vsoroka@oroinc.com>
2024-01-07 14:34:08 +0200

    BAP-22395: Refactor functional tests fixtures that depend on default organization (#37293)

--- src/Oro/Bundle/StripeBundle/Tests/Functional/DataFixtures/LoadStripeChannelData.php
+++ src/Oro/Bundle/StripeBundle/Tests/Functional/DataFixtures/LoadStripeChannelData.php
@@ -3,63 +3,58 @@
 namespace Oro\Bundle\StripeBundle\Tests\Functional\DataFixtures;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\StripeBundle\Entity\StripeTransportSettings;
-use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadUser;
 
-class LoadStripeChannelData extends AbstractFixture implements ContainerAwareInterface
+class LoadStripeChannelData extends AbstractFixture implements DependentFixtureInterface
 {
-    protected array $channelData = [
-        [
+    private array $channelData = [
+        'stripe:channel_1' => [
             'name' => 'Stripe',
             'type' => 'stripe',
             'enabled' => true,
-            'reference' => 'stripe:channel_1',
             'userMonitoring' => true,
         ],
-        [
+        'stripe:channel_2' => [
             'name' => 'Stripe2',
             'type' => 'stripe',
             'enabled' => true,
-            'reference' => 'stripe:channel_2',
             'userMonitoring' => false,
         ],
-        [
+        'stripe:channel_3' => [
             'name' => 'Stripe3',
             'type' => 'stripe',
             'enabled' => false,
-            'reference' => 'stripe:channel_3',
             'userMonitoring' => true,
         ],
     ];
 
-    protected ?ContainerInterface $container = null;
-
-    public function setContainer(ContainerInterface $container = null)
+    /**
+     * {@inheritDoc}
+     */
+    public function getDependencies(): array
     {
-        $this->container = $container;
+        return [LoadOrganization::class, LoadUser::class];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function load(ObjectManager $manager): void
     {
-        $userManager = $this->container->get('oro_user.manager');
-        $admin = $userManager->findUserByEmail(LoadAdminUserData::DEFAULT_ADMIN_EMAIL);
-        $organization = $manager->getRepository(Organization::class)->getFirst();
-
-        foreach ($this->channelData as $data) {
+        foreach ($this->channelData as $reference => $data) {
             $entity = new Channel();
             $entity->setName($data['name']);
             $entity->setType($data['type']);
             $entity->setEnabled($data['enabled']);
-            $entity->setDefaultUserOwner($admin);
-            $entity->setOrganization($organization);
+            $entity->setDefaultUserOwner($this->getReference(LoadUser::USER));
+            $entity->setOrganization($this->getReference(LoadOrganization::ORGANIZATION));
             $entity->setTransport((new StripeTransportSettings())->setUserMonitoring($data['userMonitoring']));
-            $this->setReference($data['reference'], $entity);
-
+            $this->setReference($reference, $entity);
             $manager->persist($entity);
         }
         $manager->flush();
