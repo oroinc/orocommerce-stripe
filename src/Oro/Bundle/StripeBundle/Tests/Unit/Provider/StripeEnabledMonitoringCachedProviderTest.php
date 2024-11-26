25f5288d4e2bfbdc1b3864cad74068bbe3d9e90a
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-11-26 16:40:39 +0100

    BB-24714: Enabling Stripe integration in one organization leads to Stripe cookies enablement on websites of other organizations as well (#39781)

--- src/Oro/Bundle/StripeBundle/Tests/Unit/Provider/StripeEnabledMonitoringCachedProviderTest.php
+++ src/Oro/Bundle/StripeBundle/Tests/Unit/Provider/StripeEnabledMonitoringCachedProviderTest.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\StripeBundle\Tests\Unit\Provider;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\FrontendBundle\Request\FrontendHelper;
+use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
 use Oro\Bundle\StripeBundle\Entity\Repository\StripeTransportSettingsRepository;
 use Oro\Bundle\StripeBundle\Entity\StripeTransportSettings;
 use Oro\Bundle\StripeBundle\Integration\StripeChannelType;
@@ -18,6 +19,7 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
     private FrontendHelper|MockObject $frontendHelper;
     private CacheItemPoolInterface|MockObject $cache;
     private ManagerRegistry|MockObject $manager;
+    private TokenAccessorInterface $tokenAccessor;
     private StripeEnabledMonitoringCachedProvider $provider;
 
     #[\Override]
@@ -26,11 +28,17 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
         $this->frontendHelper = $this->createMock(FrontendHelper::class);
         $this->cache = $this->createMock(CacheItemPoolInterface::class);
         $this->manager = $this->createMock(ManagerRegistry::class);
+        $this->tokenAccessor = $this->createMock(TokenAccessorInterface::class);
+        $this->tokenAccessor
+            ->expects($this->any())
+            ->method('getOrganizationId')
+            ->willReturn(3);
 
         $this->provider = new StripeEnabledMonitoringCachedProvider(
             $this->frontendHelper,
             $this->cache,
-            $this->manager
+            $this->manager,
+            $this->tokenAccessor
         );
     }
 
@@ -60,7 +68,7 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
 
         $this->cache->expects($this->once())
             ->method('getItem')
-            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING)
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING, 3))
             ->willReturn($cacheItem);
 
         $this->assertFalse($this->provider->isStripeMonitoringEnabled());
@@ -83,7 +91,7 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
 
         $this->cache->expects($this->once())
             ->method('getItem')
-            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING)
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING, 3))
             ->willReturn($cacheItem);
 
         $this->assertTrue($this->provider->isStripeMonitoringEnabled());
@@ -110,7 +118,7 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
 
         $this->cache->expects($this->once())
             ->method('getItem')
-            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING)
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING, 3))
             ->willReturn($cacheItem);
 
         $this->cache->expects($this->once())
@@ -152,7 +160,7 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
 
         $this->cache->expects($this->once())
             ->method('getItem')
-            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING)
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT_MONITORING, 3))
             ->willReturn($cacheItem);
 
         $this->cache->expects($this->once())
@@ -171,5 +179,144 @@ class StripeEnabledMonitoringCachedProviderTest extends TestCase
             ->willReturn([]);
 
         $this->assertFalse($this->provider->isStripeMonitoringEnabled());
+    }
+
+    public function testIsStripeEnabledWithBackendRequest(): void
+    {
+        $this->frontendHelper->expects($this->once())
+            ->method('isFrontendRequest')
+            ->willReturn(false);
+
+        $this->assertFalse($this->provider->isStripeEnabled());
+    }
+
+    public function testIsStripeEnabledWithDisableCache(): void
+    {
+        $this->frontendHelper->expects($this->once())
+            ->method('isFrontendRequest')
+            ->willReturn(true);
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->expects($this->once())
+            ->method('isHit')
+            ->willReturn(true);
+
+        $cacheItem->expects($this->once())
+            ->method('get')
+            ->willReturn(StripeEnabledMonitoringCachedProvider::STRIPE_DISABLE);
+
+        $this->cache->expects($this->once())
+            ->method('getItem')
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT, 3))
+            ->willReturn($cacheItem);
+
+        $this->assertFalse($this->provider->isStripeEnabled());
+    }
+
+    public function testIsStripeEnabledWithEnableCache(): void
+    {
+        $this->frontendHelper->expects($this->once())
+            ->method('isFrontendRequest')
+            ->willReturn(true);
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->expects($this->once())
+            ->method('isHit')
+            ->willReturn(true);
+
+        $cacheItem->expects($this->once())
+            ->method('get')
+            ->willReturn(StripeEnabledMonitoringCachedProvider::STRIPE_ENABLE);
+
+        $this->cache->expects($this->once())
+            ->method('getItem')
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT, 3))
+            ->willReturn($cacheItem);
+
+        $this->assertTrue($this->provider->isStripeEnabled());
+    }
+
+    public function testIsStripeEnabled(): void
+    {
+        $this->frontendHelper->expects($this->once())
+            ->method('isFrontendRequest')
+            ->willReturn(true);
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->expects($this->once())
+            ->method('isHit')
+            ->willReturn(false);
+
+        $cacheItem->expects($this->once())
+            ->method('set')
+            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_ENABLE);
+
+        $cacheItem->expects($this->once())
+            ->method('get')
+            ->willReturn(StripeEnabledMonitoringCachedProvider::STRIPE_ENABLE);
+
+        $this->cache->expects($this->once())
+            ->method('getItem')
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT, 3))
+            ->willReturn($cacheItem);
+
+        $this->cache->expects($this->once())
+            ->method('save')
+            ->with($cacheItem);
+
+        $repo = $this->createMock(StripeTransportSettingsRepository::class);
+        $this->manager->expects($this->once())
+            ->method('getRepository')
+            ->with(StripeTransportSettings::class)
+            ->willReturn($repo);
+
+        $repo->expects($this->once())
+            ->method('getEnabledSettingsByType')
+            ->with(StripeChannelType::TYPE)
+            ->willReturn([new StripeTransportSettings()]);
+
+        $this->assertTrue($this->provider->isStripeEnabled());
+    }
+
+    public function testIsStripeDisabled(): void
+    {
+        $this->frontendHelper->expects($this->once())
+            ->method('isFrontendRequest')
+            ->willReturn(true);
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->expects($this->once())
+            ->method('isHit')
+            ->willReturn(false);
+
+        $cacheItem->expects($this->once())
+            ->method('set')
+            ->with(StripeEnabledMonitoringCachedProvider::STRIPE_DISABLE);
+
+        $cacheItem->expects($this->once())
+            ->method('get')
+            ->willReturn(StripeEnabledMonitoringCachedProvider::STRIPE_DISABLE);
+
+        $this->cache->expects($this->once())
+            ->method('getItem')
+            ->with(sprintf('%s|%d', StripeEnabledMonitoringCachedProvider::STRIPE_PAYMENT, 3))
+            ->willReturn($cacheItem);
+
+        $this->cache->expects($this->once())
+            ->method('save')
+            ->with($cacheItem);
+
+        $repo = $this->createMock(StripeTransportSettingsRepository::class);
+        $this->manager->expects($this->once())
+            ->method('getRepository')
+            ->with(StripeTransportSettings::class)
+            ->willReturn($repo);
+
+        $repo->expects($this->once())
+            ->method('getEnabledSettingsByType')
+            ->with(StripeChannelType::TYPE)
+            ->willReturn([]);
+
+        $this->assertFalse($this->provider->isStripeEnabled());
     }
 }
